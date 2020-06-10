573c505cce2bc39c320d92605c993c0a3dcea8ac
Andrea <s4048705@studenti.unige.it>
2020-06-10 11:21:29 +0200

    [Cleaning] removed commented code and test scripts

--- src/classes/FeatureExtractor.py
+++ src/classes/FeatureExtractor.py
@@ -25,26 +25,6 @@ class FeatureExtractor:
                 for sentence in concept.features.annotatedSentences:
                     self.sentenceOfFocus(MyModel.dataset[MyModel.dataset.index('Probabilità condizionata')], sentence)
 
-        '''
-        i = 0
-        for concept in MyModel.dataset:
-            if concept.id == '1745121':
-                print( "concept content is: " + concept.content)
-            concept.features.sentences = nltk.tokenize.sent_tokenize(concept.content)
-            concept.features.words = nltk.word_tokenize(concept.content)
-
-            concept.features.posTags = nltk.pos_tag(concept.features.words) # Part of speech tags
-            fd = nltk.FreqDist(concept.features.posTags)    # analisi frequenziale dei pos tags
-            concept.features.entities = nltk.chunk.ne_chunk(concept.features.posTags)   # estrazione entities dal pos tag
-            # Just for quick review, to delete
-            if concept.id == '1745121':
-                print(fd['NN'] / len(concept.features.words))   # mostra percentuale nomi nel concetto
-                print('concept words: ' + str(concept.features.words))
-                print('concept pos tags: ' + str(concept.features.posTags))
-                print('concept entities: ' + str(concept.features.entities))
-                print('concept sentences: ' + str(concept.features.sentences))
-        '''
-
 
     def sentenceOfFocus(self, concept, annotatedSentence):
         # check if in sentence appears title of concept
