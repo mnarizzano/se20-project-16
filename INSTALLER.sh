ce393bb63f4f834b858bb91fb7c067aa4806cb0f
Andrea <a.parizzi@reply.it>
2020-12-13 22:45:46 +0100

    [Feat] Updated dependencies and tested installer in Ubuntu 20.04

---
+++ INSTALLER.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# NOTE: must have pip3 and git installed
+
+# Uncomment following line if having error with wrapt while installing Tensorflow
+# pip3 install wrapt --upgrade --ignore-installed
+
+# Uncomment following line if PyQt5 could not load the Qt platform plugin "xcb" in "" even though it was found.
+# sudo apt-get install libxcb-xinerama0
+
+pip3 install -r src/requirements.txt
