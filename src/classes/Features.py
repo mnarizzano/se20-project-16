bf597628ad513510686811d53105788429d4db18
Andrea <a.parizzi@reply.it>
2021-02-15 17:12:28 +0100

    [comment] added GraphMatrix and Features

--- src/classes/Features.py
+++ src/classes/Features.py
@@ -1,9 +1,44 @@
 
 class Features:
+    """ Engine
 
+    Container for all single Concept Features
+
+    ...
+
+    Attributes
+    ----------
+    conllu : Udpipe.Model
+        contains the conllu for the text of the concept
+    annotatedSentences : []
+        Contains a list where each entry is an annotated sentence of the concept
+    sentences : [string]
+        One entry for each sentence in the Concept Text
+    nounsList : [string]
+        List of nouns present in the concept Text
+    verbsList : [string]
+        List of verbs present in the concept Text
+    nounsSet : [string]
+        Set containing nouns present in the Concept Text
+    verbsSet : [string]
+        Set containing verbs present in the Concept Text
+    nounsPlain : [string]
+        List containing the lemma of each noun in the Concept Text
+    verbsPlain : [string]
+        List containing the lemma of each verb in the Concept Text
+    ldaVector : [int]
+        LDA Vector of the Concept Text
+    LDAEntropy : int
+        Entropy of the previous ldaVector
+    totalIncomingLinks : int
+        How many wikipedia pages points to this concept
+    totalOutgoingLinks : int
+        How many wikipedia pages are referenced by this concept
+
+    """
     def __init__(self):
         self.conllu = None
-        self.annotatedSentences = None # This contains a list where each entry is an annotated sentence of the concept
+        self.annotatedSentences = None
         self.sentences = []
         self.nounsList = []
         self.verbsList = []
@@ -17,10 +52,16 @@ class Features:
         self.totalOutgoingLinks = 0
 
     def get_numberOfSentences(self):
+        """Counts how many sentences are present in the concept
+        """
         return len(self.sentences)
 
     def get_annotatedSentences(self):
+        """Getter for the list of sentences
+        """
         return self.sentences
 
     def get_LDAVector(self):
+        """Getter for the LDA Vector of the Text of the Concept
+        """
         return self.ldaVector
--- src/classes/GraphMatrix.py
+++ src/classes/GraphMatrix.py
@@ -2,26 +2,50 @@ from Settings import Settings
 from Model import Model
 
 class GraphMatrix:
+    """ GraphMatrix
 
+    Linked List of labels between pairs of Concepts
+
+    ...
+
+    Attributes
+    ----------
+    unknown : int
+        Define the value used to express an unknown prerequisite relation
+    isPrereq : int
+        Define the value used to express a known prerequisite
+    notPrereq : int
+        Define the value used to express the known absence of a prerequisite
+    matrix : {concept_1: {concept_X: bool, concept_Y: bool, ...}, concept_2: {}, ....}
+        Linked list wich tracks prereq existence between pairs of Concepts
+    domains : [string]
+        tracks what are the domain of the run
+    numberOfPrereqs : int
+        Counter that tracks how many prereq relations exist
+    numberOfNonPrereqs : int
+        Counter that tracks how many non-prereq relations exist
+    unknownPrereqs : int
+        Counter that tracks how many unknown prereq relations exist
+    """
     unknown = -1
     isPrereq = 1
     notPrereq = 0
+    matrix = {}
     domains = {}
+    numberOfPrereqs = 0
+    numberOfNonPrereqs = 0
+    unknownPrereqs = 0
 
     def __init__(self, domains):
-        # -1 if prereq not present (in dataset), 0 if prereq explicitly 0 (in dataset), 1 if prereq
-        #self.matrix = [[{k: self.unknown for k in domains} for i in range(len(Model.dataset))] for j in range(len(Model.dataset))]
-        self.matrix = {}
         self.domains = domains
-        self.numberOfPrereqs = 0
-        self.numberOfNonPrereqs = 0
-        self.unknownPrereqs = 0
-
 
     def getStatistics(self):
         return [self.numberOfPrereqs, self.numberOfNonPrereqs, self.unknownPrereqs]
 
     def addPrereq(self, conceptA, conceptB, value, domain):
+        """Given a pair of Concepts, their domain and the prereq
+           relation between them adds it to the LinkedList
+        """
         if not self.matrix.__contains__(conceptA):
             self.matrix[conceptA] = {}
         if not self.matrix[conceptA].__contains__(conceptB):
@@ -43,6 +67,8 @@ class GraphMatrix:
                     "Dataset warning: found duplicated annotation for  '" + conceptA + ', ' + conceptB + "' in '"+domain+"'")
 
     def plotGraph(self):
+        """Plots the prereq List in matrix Form
+        """
         for row in range(len(self.matrix[:][0])):
             row2string = ''
             for col in range(len(self.matrix[0][:])):
@@ -50,15 +76,23 @@ class GraphMatrix:
             Settings.logger.debug(row2string + "\n")
 
     def getPrereqs(self):
+        """Returns all prereqs in ListForm
+        """
         return [prereq for prereq in self.matrix]
 
     def getPostreqs(self, prereq):
+        """Given a pre-Concepts returns all post-Concepts that rely on it
+        """
         return [postreq for postreq in self.matrix[prereq]]
 
     def getDomains(self, prereq, postreq):
+        """Returns a list of all the Domains present
+        """
         return [domain for domain in self.matrix[prereq][postreq]]
 
     def plotPrereqs(self):
+        """Console log all prereq relations
+        """
         Settings.logger.debug(
             "Total concepts: " + str(len(Model.dataset)) + ". Total possible Prerequisites: " + str(len(Model.dataset)**2))
         Settings.logger.debug(
@@ -75,4 +109,6 @@ class GraphMatrix:
                                           str(self.matrix[prereq][postreq][domain]))
 
     def getPrereq(self, conceptA, conceptB, domain):
+        """Given a pair of concepts and their domain return the prereq relation between them
+        """
         return self.matrix[conceptA][conceptB][domain]
